3b85c31a33fc90fae99656ea6c3f38d2abbbcbd1
aprilJade <apriljade0831@gmail.com>
2022-05-22 21:21:38 +0900

    Move gtk3 to gtk4

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,28 @@
+use gtk4 as gtk;
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow};
+use gtk::{Application, ApplicationWindow, Button};
 
 fn main() {
-    let app = Application::builder()
-        .application_id("org.example.HelloWorld")
+    let application = Application::builder()
+        .application_id("com.example.FirstGtkApp")
         .build();
 
-    app.connect_activate(|app| {
-        // We create the main window.
-        let win = ApplicationWindow::builder()
+    application.connect_activate(|app| {
+        let window = ApplicationWindow::builder()
             .application(app)
+            .title("Calculator")
             .default_width(480)
             .default_height(800)
-            .title("Calculator")
             .build();
 
-        // Don't forget to make all widgets visible.
-        win.show_all();
+        let button = Button::with_label("Click me!");
+        button.connect_clicked(|_| {
+            eprintln!("Clicked!");
+        });
+        window.set_child(Some(&button));
+
+        window.show();
     });
 
-    app.run();
+    application.run();
 }
