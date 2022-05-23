307368a9185595c88d5fa78b5e6ecd26e26bb2ad
aprilJade <apriljade0831@gmail.com>
2022-05-23 15:44:26 +0900

    Implement some button events...

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use gtk4 as gtk;
+use gtk::prelude::*;
 use glib::clone;
 use gtk::glib;
-use gtk::prelude::*;
 
 fn main() {
     let application = gtk::Application::new(
@@ -22,13 +22,12 @@ fn build_ui(application: &gtk::Application) {
     let grid = gtk::Grid::builder()
         .margin_start(6)
         .margin_end(6)
-        .margin_top(80)
+        .margin_top(6)
         .margin_bottom(6)
         .halign(gtk::Align::Center)
         .valign(gtk::Align::Center)
         .row_spacing(0)
         .column_spacing(0)
-        .can_focus(false)
         .build();
     window.set_child(Some(&grid));
 
@@ -43,9 +42,9 @@ fn build_ui(application: &gtk::Application) {
     let button_7 = gtk::Button::with_label("7");
     let button_8 = gtk::Button::with_label("8");
     let button_9 = gtk::Button::with_label("9");
-    let button_cancel = gtk::Button::with_label("AC");
+    let button_clear = gtk::Button::with_label("AC");
     let button_sign = gtk::Button::with_label("+/-");
-    let button_modular = gtk::Button::with_label("%");
+    let button_percent = gtk::Button::with_label("%");
     let button_divider = gtk::Button::with_label("÷");
     let button_mutiplier = gtk::Button::with_label("x");
     let button_minus = gtk::Button::with_label("-");
@@ -53,29 +52,119 @@ fn build_ui(application: &gtk::Application) {
     let button_equal = gtk::Button::with_label("=");
     let button_point = gtk::Button::with_label(".");
 
-    grid.attach(&button_cancel, 0, 0, btn_len, btn_len);
-    grid.attach(&button_sign, 1, 0, btn_len, btn_len);
-    grid.attach(&button_modular, 2, 0, btn_len, btn_len);
-    grid.attach(&button_divider, 3, 0, btn_len, btn_len);
-    
-    grid.attach(&button_7, 0, 1, btn_len, btn_len);
-    grid.attach(&button_8, 1, 1, btn_len, btn_len);
-    grid.attach(&button_9, 2, 1, btn_len, btn_len);
-    grid.attach(&button_mutiplier, 3, 1, btn_len, btn_len);
-    
-    grid.attach(&button_4, 0, 2, btn_len, btn_len);
-    grid.attach(&button_5, 1, 2, btn_len, btn_len);
-    grid.attach(&button_6, 2, 2, btn_len, btn_len);
-    grid.attach(&button_minus, 3, 2, btn_len, btn_len);
+    let text_view = gtk::Entry::builder()
+        .build();
 
-    grid.attach(&button_1, 0, 3, btn_len, btn_len);
-    grid.attach(&button_2, 1, 3, btn_len, btn_len);
-    grid.attach(&button_3, 2, 3, btn_len, btn_len);
-    grid.attach(&button_plus, 3, 3, btn_len, btn_len);
+    grid.attach(&text_view, 0, 0, 5, 1);
+
+    grid.attach(&button_clear, 0, 1, btn_len, btn_len);
+    grid.attach(&button_sign, 1, 1, btn_len, btn_len);
+    grid.attach(&button_percent, 2, 1, btn_len, btn_len);
+    grid.attach(&button_divider, 3, 1, btn_len, btn_len);
     
-    grid.attach(&button_0, 0, 4, btn_len * 2, btn_len);
-    grid.attach(&button_point, 2, 4, btn_len, btn_len);
-    grid.attach(&button_equal, 3, 4, btn_len, btn_len);
+    grid.attach(&button_7, 0, 2, btn_len, btn_len);
+    grid.attach(&button_8, 1, 2, btn_len, btn_len);
+    grid.attach(&button_9, 2, 2, btn_len, btn_len);
+    grid.attach(&button_mutiplier, 3, 2, btn_len, btn_len);
+    
+    grid.attach(&button_4, 0, 3, btn_len, btn_len);
+    grid.attach(&button_5, 1, 3, btn_len, btn_len);
+    grid.attach(&button_6, 2, 3, btn_len, btn_len);
+    grid.attach(&button_minus, 3, 3, btn_len, btn_len);
+
+    grid.attach(&button_1, 0, 4, btn_len, btn_len);
+    grid.attach(&button_2, 1, 4, btn_len, btn_len);
+    grid.attach(&button_3, 2, 4, btn_len, btn_len);
+    grid.attach(&button_plus, 3, 4, btn_len, btn_len);
+    
+    grid.attach(&button_0, 0, 5, btn_len * 2, btn_len);
+    grid.attach(&button_point, 2, 5, btn_len, btn_len);
+    grid.attach(&button_equal, 3, 5, btn_len, btn_len);
 
     window.show();
+
+    button_0.connect_clicked(clone!(@weak text_view => move |_btn| {
+        let text = format!("{}{}", text_view.text().as_str(), "0");
+        text_view.set_text(text.as_str());
+    }));
+
+    button_1.connect_clicked(clone!(@weak text_view => move |_btn| {
+        let text = format!("{}{}", text_view.text().as_str(), "1");
+        text_view.set_text(text.as_str());
+    }));
+
+    button_2.connect_clicked(clone!(@weak text_view => move |_btn| {
+        let text = format!("{}{}", text_view.text().as_str(), "2");
+        text_view.set_text(text.as_str());
+    }));
+
+    button_3.connect_clicked(clone!(@weak text_view => move |_btn| {
+        let text = format!("{}{}", text_view.text().as_str(), "3");
+        text_view.set_text(text.as_str());
+    }));
+
+    button_4.connect_clicked(clone!(@weak text_view => move |_btn| {
+        let text = format!("{}{}", text_view.text().as_str(), "4");
+        text_view.set_text(text.as_str());
+    }));
+
+    button_5.connect_clicked(clone!(@weak text_view => move |_btn| {
+        let text = format!("{}{}", text_view.text().as_str(), "5");
+        text_view.set_text(text.as_str());
+    }));
+
+    button_6.connect_clicked(clone!(@weak text_view => move |_btn| {
+        let text = format!("{}{}", text_view.text().as_str(), "6");
+        text_view.set_text(text.as_str());
+    }));
+
+    button_7.connect_clicked(clone!(@weak text_view => move |_btn| {
+        let text = format!("{}{}", text_view.text().as_str(), "7");
+        text_view.set_text(text.as_str());
+    }));
+
+    button_8.connect_clicked(clone!(@weak text_view => move |_btn| {
+        let text = format!("{}{}", text_view.text().as_str(), "8");
+        text_view.set_text(text.as_str());
+    }));
+
+    button_9.connect_clicked(clone!(@weak text_view => move |_btn| {
+        let text = format!("{}{}", text_view.text().as_str(), "9");
+        text_view.set_text(text.as_str());
+    }));
+
+    button_clear.connect_clicked(clone!(@weak text_view => move |_btn| {
+        text_view.set_text("");
+    }));
+    /*
+
+    let button_sign = gtk::Button::with_label("+/-");
+    let button_percent = gtk::Button::with_label("%");
+    let button_divider = gtk::Button::with_label("÷");
+    let button_mutiplier = gtk::Button::with_label("x");
+    let button_minus = gtk::Button::with_label("-");
+    let button_plus = gtk::Button::with_label("+");
+    let button_equal = gtk::Button::with_label("=");
+    let button_point = gtk::Button::with_label(".");
+}
+*/
+    button_sign.connect_clicked(clone!(@weak text_view => move |_btn| {
+        let ch = text_view.text().as_str().chars().nth(0).unwrap();
+        let str = String::from(text_view.text());
+        if ch == '-' {
+            let mut chars = str.chars();
+            chars.next();
+            text_view.set_text(chars.as_str());
+        } else {
+            let text = format!("{}{}", "-", text_view.text().as_str());
+            text_view.set_text(text.as_str());
+        }
+    }));
+
+    button_percent.connect_clicked(clone! (@weak text_view => move |_btn| {
+        let str = String::from(text_view.text());
+        let mut value = str.parse::<f64>().unwrap();
+        value *= 0.01;
+        text_view.set_text(value.to_string().as_str());
+    }));
 }
